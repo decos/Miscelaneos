7d8108474820f3d7e95a28e351cb9b2a6502b522
diego <diego@diego-lenovo.cpe.tdp.com>
2017-09-09 17:53:43 -0500

    DIRECTIVAS PERSONALIZADAS

---
+++ src/app/directives/resaltado.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appResaltado]'
+})
+export class ResaltadoDirective {
+
+  constructor( private el:ElementRef ) {
+    console.log("Directiva llamada");
+    //el.nativeElement.style.backgroundColor = "yellow";
+  }
+
+  //Esta variable viene de afuera
+  @Input("appResaltado") nuevoColor:string;
+
+  @HostListener('mouseenter') mouseEntro(){
+    console.log( this.nuevoColor );
+    this.resaltar( this.nuevoColor || "yellow" );
+    //this.el.nativeElement.style.backgroundColor = "yellow";
+  }
+
+  @HostListener('mouseleave') mouseSalio(){
+    this.resaltar( null );
+    //this.el.nativeElement.style.backgroundColor = null;
+  }
+
+  private resaltar( color: string ){
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+}
