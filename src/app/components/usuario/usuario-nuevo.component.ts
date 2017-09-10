44b63642d63e448605ca9847dba849f83a67d372
diego <diego@diego-lenovo.cpe.tdp.com>
2017-09-10 03:11:57 -0500

    RUTAS Y RUTAS HIJAS

---
+++ src/app/components/usuario/usuario-nuevo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-nuevo',
+  template: `
+    <p>
+      usuario-nuevo Works!
+    </p>
+  `,
+})
+export class UsuarioNuevoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
